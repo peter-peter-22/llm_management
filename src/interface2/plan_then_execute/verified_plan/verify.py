87345a52d2f7dfb0f23779f4c80a046b44d5a20b
peter <magyarpeter222@gmail.com>
2026-01-19 00:27:06 +0100

    fix the verifier

--- src/interface2/plan_then_execute/verified_plan/verify.py
+++ src/interface2/plan_then_execute/verified_plan/verify.py
@@ -22,11 +22,15 @@ class Rating(BaseModel):
 
 agent = ToolLoop[Rating](llm, response_model=Rating, tool_choice="required")
 
+baseline_requirements = [
+    "Do not query the database to retrieve all record, always use filters or limits."
+]
+
 
 def verify_plan(goal: str, plan_steps: list[str], requirements: list[str]):
     plan_text = "The steps:\n" + "\n".join([f"-{step}\n" for step in plan_steps])
     requirements_text = f"The goal:\n{goal}"
-    requirements_text += "\nRequirements:\n" + "\n".join([f"-{req}\n" for req in requirements])
+    requirements_text += "\nRequirements:\n" + "\n".join([f"-{req}\n" for req in requirements + baseline_requirements])
     messages = [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": plan_text},
@@ -49,6 +53,16 @@ def _test():
     ]
     res = verify_plan(goal, steps, reqs)
     print(res)
+
+    wrong_steps = [
+        'Query the SQL database of Fakesoft for a list of all projects',
+        'Present the results from step 1 to a text transformation agent for semantic processing, converting the project descriptions into concise briefs that highlight the main goal or objective of each project.',
+        'After receiving the processed briefs from the text transformation agent, present the list of the 3 most recent projects and their brief descriptions to the user as the final answer.'
+    ]
+    res = verify_plan(goal, steps, reqs)
+    print("Correct", res)
+    res = verify_plan(goal, wrong_steps, reqs)
+    print("Incorrect", res)
 
 
 if __name__ == "__main__":
